f9dd826abd27a6a1ba77ae341a0d1cea580a7005
maksk <maksk@KKK>
2023-10-14 12:08:55 +0200

    Add project files.

---
+++ GAYPARTY/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string grades = Console.ReadLine();
+        string[] newstring = grades.Split(',');
+        int[] numbers = new int[newstring.Length];
+        bool ValueReq;
+        for (int i = 0; i < newstring.Length; i++)
+        {
+            numbers[i] = int.Parse(newstring[i]);
+        }
+        if (numbers[0] == 5 && numbers[1] == 5 && numbers[2] == 5 && numbers[3] == 5)
+        {
+            bool ValueReq = true;
+
+        }
+        else
+        {
+            bool ValueReq = false;
+ 
+        }
+        foreach (int x in numbers)
+        {
+            if (x == 2)
+            {
+                Console.WriteLine("Ну что, студент, пора долг Родине отдать");
+                return;
+            }
+
+            if (x == 3)
+            {
+                Console.WriteLine("Прощай стипендия!");
+                return;
+            }
+
+
+
+            else if (x != 2 && x != 3 & ValueReq == false)
+            {
+                Console.WriteLine("Живи пока, через полгода увидимся");
+                return;
+            }
+            else continue;
+
+
+
+        }
+        if (numbers[0] == 5 && numbers[1] == 5 && numbers[2] == 5 && numbers[3] == 5)
+        {
+            Console.WriteLine("О, отличник появился! На олимпиаду пойдешь");
+
+        }
+    }
+        
+
+        
+        
+}
