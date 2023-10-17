5a5d2a2cd1295ce011eee747e3778130962bdbde
Schub Bergman <maks.kon.92@mail.ru>
2023-10-17 14:05:22 +0200

    Reverse

--- GAYPARTY/Program.cs
+++ GAYPARTY/Program.cs
@@ -16,13 +16,21 @@ public class MainClass
 
 
         */
-        string s = "124124124";
+        string s = Console.ReadLine();
         int[] ints = new int[s.Length];
         for (int i = 0; i < ints.Length; i++)
         {
-            int n = (int)char.GetNumericValue(s[i]);
-            ints[i] = n;
-            Console.WriteLine(
+            int n = (int)char.GetNumericValue(s[i]); // создается рандомный n,
+            ints[i] = n;                       // , а потом его вводят в каждый элемент инта i 
+        }
+        int maxValue = ints.Max();
+        int minValue = ints.Min();
+        Console.Write(minValue);
+        Console.Write(maxValue);
+
+        Array.Reverse(ints);
+        for ( int i = 0; i < ints.Length; ++i )
+        {
         }
     }
 }
