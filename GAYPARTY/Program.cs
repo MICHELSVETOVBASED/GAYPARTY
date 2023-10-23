362baccf7fe2a5dc44a7896ea400c703866e43b2
Schub Bergman <maks.kon.92@mail.ru>
2023-10-23 21:21:19 +0200

    To one day

--- GAYPARTY/Program.cs
+++ GAYPARTY/Program.cs
@@ -1,63 +1,56 @@
-﻿public static bool IsTextIncludesChar(string text)
+﻿public static void Factorize(int number)
 {
-    public static void Main()
+    /*List<int> newlist = new List<int>();
+
+    for (int i = 1; i <= number; i++)
     {
-        string grades = Console.ReadLine();
-        string[] newstring = grades.Split(',');
-        int[] numbers = new int[newstring.Length];
-        bool ValueReq;
-        for (int i = 0; i < newstring.Length; i++)
+        if (number % i == 0)
         {
-            numbers[i] = int.Parse(newstring[i]);
-        }
-        if (numbers[0] == 5 && numbers[1] == 5 && numbers[2] == 5 && numbers[3] == 5)
-        {
-            bool ValueReq = true;
-
-        }
-        else
-        {
-            bool ValueReq = false;
- 
-        }
-        foreach (int x in numbers)
-        {
-            if (x == 2)
-            {
-                Console.WriteLine("Ну что, студент, пора долг Родине отдать");
-                return;
-            }
-
-            if (x == 3)
-            {
-                Console.WriteLine("Прощай стипендия!");
-                return;
-            }
-
-
-
-            else if (x != 2 && x != 3 & ValueReq == false)
-            {
-                Console.WriteLine("Живи пока, через полгода увидимся");
-                return;
-            }
-            else continue;
-
-
-
-        }
-        if (numbers[0] == 5 && numbers[1] == 5 && numbers[2] == 5 && numbers[3] == 5)
-        {
-            Console.WriteLine("О, отличник появился! На олимпиаду пойдешь");
-            Console.WriteLine("О, отличник появился! На олимпиаду пойдешь"); Console.WriteLine("О, отличник появился! На олимпиаду пойдешь");
-            Console.WriteLine("О, отличник появился! На олимпиаду пойдешь");
-            Console.WriteLine("О, отличник появился! На олимпиаду пойдешь");
-
-
+            newlist.Add(i);
         }
     }
-        
+    for (int k = 0; k < newlist.Count / 2; k++)
+    {
 
-        
-        
+        Console.WriteLine($"{number} = {newlist[k]} * {newlist[newlist.Count - k - 1]}");
+    }*/
+    int[] dividers = new int[number];
+    int[] dividers1 = new int[number];
+    int N = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            dividers[i] = i;
+            N++;
+            int c = number / i;
+            dividers1[i] = c;
+        }
+    }
+    int[] arr = new int[N];
+    int j = 0;
+    for (int i = 0; i <= arr.Length; i++)
+    {
+        if (dividers[i] != 0)
+        {
+            arr[j] = dividers[i];
+            j++;
+        }
+
+    }
+    int[] arr1 = new int[N];
+    int l = 0;
+    for (int i = 0; i <= arr1.Length; i++)
+    {
+        if (dividers1[i] != 0)
+        {
+            arr1[l] = dividers1[i];
+            l++;
+        }
+
+    }
+    for (int k = 0; k <= arr.Length; k++)
+    {
+       return Console.WriteLine($"{number} = {arr1[k]} * {arr[k]}");
+    }
 }
